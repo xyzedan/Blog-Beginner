ee3251fb3c58eab1d5d1e04a5073658ed0712d6d
xyzedn <ahmadzaid13.az@gmail.com>
2024-12-08 06:38:16 +0700

    Route: Setup

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+
+Route::view('/about', 'about')->name('about');
+Route::resource('articles', ArticleController::class)->except(['index', 'show']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('tags', TagController::class);
+
 });
 
 require __DIR__.'/auth.php';
